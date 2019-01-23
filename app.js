be77f4b398342e9f66abb058e9e7acd806c3ad55
jeanjosephgeorge <jean.joseph@outlook.com>
2019-01-23 11:20:13 -0600

    set up ejs

--- app.js
+++ app.js
@@ -2,7 +2,10 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine','ejs');
+app.set('views','views')
 
+// app.use(require('./routes/index'))
 
 // Server Listening on Port 5000
 app.listen(5000, ()=>{
