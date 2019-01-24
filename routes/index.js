a46bf5556a5204b4ade19f5edeb811c5acc1a1fb
Anuj <anujsaheba@gmail.com>
2019-01-23 21:49:51 -0600

    merge-conflicts resolved

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,9 @@ var express = require('express')
 var router = express.Router()
 
 router.get('/',(req,res)=>{
-<<<<<<< HEAD
-    res.send('Hello Werld')
-    
-=======
     res.render('index',{
         pageTitle: 'Welcome!'
     })
->>>>>>> 9462421123d4a7873d020bcaba58ef124fa6f720
 })
 
 module.exports = router;
