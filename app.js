b5c9e0c8c038a39cd2e0ac4c08105463a73d7664
Anuj <anujsaheba@gmail.com>
2019-01-23 18:21:12 -0600

    update

--- app.js
+++ app.js
@@ -2,7 +2,36 @@
 const express = require('express');
 const app = express();
 
+// API AXIOS - npm install axios
+const axios = require ('axios');
 
+// API Call
+// News API
+// axios.get('https://newsapi.org/v2/top-headlines?' + 'country=us&' + 'apiKey=6afdb077c1994657ab9284882c5c296e')
+//     .then((res)=>{
+//         console.log(res.data);
+//     })
+//     .catch((err)=>{
+//         console.log(err);
+//     });
+
+// Youtube API - Setup URL
+// var youtubeAPIkey = 'AIzaSyAAY8PfuRZux7Uw06bjgWK6MCEz6Uho1TE';
+// var query = 'kobe+bryant';
+// axios.get('https://www.googleapis.com/youtube/v3/search?part=snippet&q=' + query + '&type=video&key=' + youtubeAPIkey)
+//     .then((res)=>{
+//         console.log(res.data.items)
+//     })
+//     .catch((err)=>{
+//         console.log(err)
+//     });
+
+
+
+
+app.get('/', (req,res)=>{
+    res.send('homepage');
+})
 
 // Server Listening on Port 5000
 app.listen(5000, ()=>{
