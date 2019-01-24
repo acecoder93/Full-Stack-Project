d263d8302c5c2eea31e0ee9ee5ce5c95e4c267d9
RMenjivar0204 <raul.menjivar4@gmail.com>
2019-01-24 09:14:25 -0600

    weather

--- app.js
+++ app.js
@@ -2,12 +2,47 @@
 const express = require('express');
 const app = express();
 
+//API AXIOS
+const axios = require('axios');
+
+//EJS SETUP
 app.set('view engine','ejs');
 app.set('views','views')
 
+app.use(express.static('public'));
+
 app.use(require('./routes/index'))
 
+
+//WEATHER API
+var city = 'Houston'
+
+var apiKey = '&&APIKEY=0312eb22f231c98c931a5137466c8e3f';
+
+var unitFahrenheit = '&&units=imperial';
+
+
+
+
+
+//AXIOS WEATHER API
+axios.get('http://api.openweathermap.org/data/2.5/weather?lat=29.7604&lon=-95.3698' + apiKey + unitFahrenheit)
+    .then((res)=>{
+        console.log(res.data);
+    })
+    .catch((error)=>{
+        console.log(error);
+    });
+
+
+
+
+
+
+
+
+
 // Server Listening on Port 5000
-app.listen(5000, ()=>{
-    console.log('Listening on port 5000');
+app.listen(4000, ()=>{
+    console.log('Listening on port 4000');
 });
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var router = express.Router()
 
 router.get('/',(req,res)=>{
-    res.send('Hello Werld')
+    res.send('Hello World')
 })
 
 module.exports = router;
