1b91eb6ed9e5920432494125060e4258b4277c2c
Anuj <anujsaheba@gmail.com>
2019-01-25 11:48:21 -0600

    updated youtube API jQuery

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ const axios = require('axios');
 
 
 router.get('/',(req,res)=>{
+    res.render('index')
+})
 
 // News API
     // var articles = '';
@@ -37,24 +39,24 @@ router.get('/',(req,res)=>{
 
 
 
-    // Youtube API - Setup URL
-    var output = '';
-    var youtubeAPIkey = 'AIzaSyAAY8PfuRZux7Uw06bjgWK6MCEz6Uho1TE';
-    // Query needs to be based off of user input
-    var query = 'design';
-    axios.get('https://www.googleapis.com/youtube/v3/search?part=snippet&q=' + query + '&type=video&key=' + youtubeAPIkey)
-        .then((res) => {
-            output = res.data.items[0].id.videoId
-            // Returning the output of JSON file to the next promise
-            return output
-        }).then((output) =>{
-            // Utilizing output from previous promise to render onto next page once API call is complete
-            res.render('index', {
-                pageTitle: 'Welcome!',
-                videoLink: output})
-        }).catch((err)=>{
-            console.log(err)
-        })
-    })
+    // // Youtube API - Setup URL
+    // var output = '';
+    // var youtubeAPIkey = 'AIzaSyAAY8PfuRZux7Uw06bjgWK6MCEz6Uho1TE';
+    // // Query needs to be based off of user input
+    // var query = 'design';
+    // axios.get('https://www.googleapis.com/youtube/v3/search?part=snippet&q=' + query + '&type=video&key=' + youtubeAPIkey)
+    //     .then((res) => {
+    //         output = res.data.items[0].id.videoId
+    //         // Returning the output of JSON file to the next promise
+    //         return output
+    //     }).then((output) =>{
+    //         // Utilizing output from previous promise to render onto next page once API call is complete
+    //         res.render('index', {
+    //             pageTitle: 'Welcome!',
+    //             videoLink: output})
+    //     }).catch((err)=>{
+    //         console.log(err)
+    //     })
+    // })
             
 module.exports = router;
