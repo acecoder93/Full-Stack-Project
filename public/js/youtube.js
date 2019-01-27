7d1b816b54303416f7b75b852ae30c55cc9adfc7
Anuj <anujsaheba@gmail.com>
2019-01-26 20:56:48 -0600

    implemented youtube API

--- public/js/youtube.js
+++ public/js/youtube.js
@@ -1,29 +1,27 @@
+$(function(){
 
-// console.log('Here is the index.js page');
-
-
-// $(function(){
-
-//         // Youtube API 
-//         var youtubeAPIkey = 'AIzaSyAAY8PfuRZux7Uw06bjgWK6MCEz6Uho1TE';
-//         // Query needs to be based off of user input
-//         var query = 'design';
-//         var url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&q=' + query + '&type=video&key=' + youtubeAPIkey;
-
-//         $.get(url)
-//         .done((result)=>{
-//             var output = result.data.items[0].id.videoId;
-//             console.log(output);  
-//             var $youtubeDiv =$('#youtubeDivID');
-//             var $youtubeFrame =  $('<iframe>', {
-//                 'width': '1000%',
-//                 'height': 'auto',
-//                 'src': `https://www.youtube.com/embed/${output}`,
-//                 'frameborder': '0',
-//                 'allow': 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
-//             });
-//             return $youtubeDiv.append($youtubeFrame);
-//             // console.log(output);
-//             // console.log($youtubeDiv);
-//         })
-//     })
+    // Youtube API 
+    var youtubeAPIkey = 'AIzaSyAAY8PfuRZux7Uw06bjgWK6MCEz6Uho1TE';
+    var query = 'design';
+    var url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&q=' + query + '&type=video&key=' + youtubeAPIkey;
+    
+    $.get(url)
+    .done((result)=>{
+        // console.log(result);
+        var output = result.items[0].id.videoId
+        console.log(output);  
+    
+        var $youtubeDiv =$('#youtubeDivID');
+        var $youtubeDiv =$('#youtubeDivID');
+        var $youtubeFrame =  $('<iframe>', {
+            'width': '500px',
+            'height': '281px',
+            'src': `https://www.youtube.com/embed/${output}`,
+            'frameborder': '0',
+            'allow': 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+        });
+        return $youtubeDiv.append($youtubeFrame);
+        // console.log(output);
+        // console.log($youtubeDiv);
+    })
+    })
