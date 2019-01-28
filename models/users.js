e6b10e31d7a7001ec73872ab8b7e902f7230593d
Anuj <anujsaheba@gmail.com>
2019-01-28 11:54:31 -0600

    updated

--- models/users.js
+++ models/users.js
@@ -9,6 +9,7 @@ module.exports = (sequelize, DataTypes) => {
     zipcode: DataTypes.STRING
   }, {});
   users.associate = function(models) {
+    
     // associations can be defined here
   };
   return users;
