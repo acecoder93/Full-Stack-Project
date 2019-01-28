682e8022cf679f3d682ddf0481a09d9670b7f4cc
Anuj <anujsaheba@gmail.com>
2019-01-28 13:23:05 -0600

    youtube user query in-progress

--- public/js/youtube.js
+++ public/js/youtube.js
@@ -1,20 +1,29 @@
 $(function(){
 
-    $()
+    var query =  $('#youtubeButton').click(function(){
+        var userResponse = $('#youtubeQuery').val();
+        console.log(userResponse);
+        return userResponse
+        
+    })
 
 
+    $(function youtubeGo(query){
     // Youtube API 
     var youtubeAPIkey = 'AIzaSyAAY8PfuRZux7Uw06bjgWK6MCEz6Uho1TE';
-    var query =  $('#youtubeQuery').val();
+    // var query =  $('#youtubeButton').click(function(){
+    //     var userResponse = $('#youtubeQuery').val();
+    //     console.log(userResponse);
+    //     return userResponse
+    // })
+ 
     console.log(query);
     // var query = 'javascript';
     var url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&q=' + query + '&type=video&key=' + youtubeAPIkey;
     
     $.get(url)
     .done((result)=>{
-        // console.log(result);
         var output = result.items[0].id.videoId
-    
         var $youtubeDiv =$('#youtubeDivID');
         var $youtubeFrame =  $('<iframe>', {
             'width': '500px',
@@ -24,7 +33,12 @@ $(function(){
             'allow': 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
         });
         return $youtubeDiv.append($youtubeFrame);
-        // console.log(output);
-        // console.log($youtubeDiv);
+
     })
     })
+
+    youtubeGo(query)
+
+
+
+})
