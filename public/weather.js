3126c657ec6f523f2896ec4a13c06449efb33e49
Steven Fisher <steven.fisher22@gmail.com>
2019-01-29 12:28:28 -0600

    weather API adjustments

--- public/weather.js
+++ public/weather.js
@@ -2,28 +2,49 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 
-
+//////////
+// OLD
+//////////
 
 //WEATHER API
-var city = 'Houston'
+// var city = 'Houston'
+// var apiKey = '&&APIKEY=0312eb22f231c98c931a5137466c8e3f';
+// var unitFahrenheit = '&&units=imperial';
 
-var apiKey = '&&APIKEY=0312eb22f231c98c931a5137466c8e3f';
+// //AXIOS
+// axios.get('http://api.openweathermap.org/data/2.5/weather?lat=29.7604&lon=-95.3698' + apiKey + unitFahrenheit)
+//     .then((res)=>{
+//         console.log(res.data);
+//     })
+//     .catch((error)=>{
+//         console.log(error);
+//     });
 
-var unitFahrenheit = '&&units=imperial';
+
+//////////
+// NEW
+//////////
+
+//WEATHER API
+var url = "http://api.openweathermap.org/data/2.5/weather?zip=";
+var apiKey = '&APPID=0312eb22f231c98c931a5137466c8e3f';
+var zipcode = '77095';
+var unitFahrenheit = ',us&units=imperial';
 
 //AXIOS
-axios.get('http://api.openweathermap.org/data/2.5/weather?lat=29.7604&lon=-95.3698' + apiKey + unitFahrenheit)
-    .then((res)=>{
-        console.log(res.data);
+axios.get(url + zipcode + unitFahrenheit + apiKey)
+    .then((result)=>{
+        console.log(result.data);
+        var fahrenheit = result.main.temp;
+        var degF = Math.floor(fahrenheit);
+        var degree = '&deg;F';
+        var total = degF + degree;
+        total.replace(`/\s/g, ''`);
+        var city = result.name;
+        $('#navTemp').append(`${total} | ${city}`);
     })
     .catch((error)=>{
         console.log(error);
-    });
-
-
-
-
-
-
+});
 
 module.exports = router;
