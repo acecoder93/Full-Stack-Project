77fdd584e229b88490e03bb03a7f70acce631ddc
jeanjosephgeorge <jean.joseph@outlook.com>
2019-02-13 23:03:53 -0600

    white space

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -4,13 +4,9 @@ var router = express.Router();
 
 router.get('/dashboard',(req,res)=>{
 
-
-
-    
     res.render('dashboard',{
         pageTitle: 'Dashboard'
     })
 });
-
 
 module.exports = router;
